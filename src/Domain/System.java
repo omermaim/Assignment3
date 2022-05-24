12b9cc71ac00f51e80e6b0a9632c797c388d034a
omermaim <omermaimon2@gmail.com>
2022-05-24 15:30:19 +0300

    tomer was addeed

--- src/Domain/System.java
+++ src/Domain/System.java
@@ -1,5 +1,6 @@
 package Domain;
 
+import java.sql.Date;
 import java.util.ArrayList;
 
 public class System {
@@ -16,4 +17,15 @@ public class System {
 
     }
 
+    public boolean Login(String login_name, String login_password) {
+        return false;
+    }
+
+    public boolean addReferee(String name, int id, Date birthdate, int phone) {
+        return false;
+    }
+
+    public boolean gamePlacement(int hometeam_id, int guestteam_id, ArrayList<Referee> threereferees) {
+        return false;
+    }
 }
--- src/Service/UserService.java
+++ src/Service/UserService.java
@@ -2,9 +2,22 @@ package Service;
 import Domain.*;
 import Domain.System;
 
+import java.sql.Date;
+import java.util.ArrayList;
+
 public class UserService {
-    System Service_controller = System.getinstance();
+    System Service_controller;
 
-
-
+    public UserService(System service_controller) {
+        Service_controller = System.getinstance();
+    }
+    public boolean Login(String login_name,String login_password){
+        return Service_controller.Login(login_name,login_password);
+    }
+    public boolean addReferee(String name, int id, Date birthdate,int phone){
+        return Service_controller.addReferee(name,id,birthdate,phone);
+    }
+    public boolean gamePlacement(int hometeam_id, int guestteam_id, ArrayList<Referee> threereferees,Date date){
+        return Service_controller.gamePlacement(hometeam_id,guestteam_id,threereferees);
+    }
 }
