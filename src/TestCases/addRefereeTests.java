6c449116aa5ab74fe27bea8fbb7121ebe053e079
hodisan94 <dhodisan@gmail.com>
2022-05-24 23:39:09 +0300

    testing testing

--- src/TestCases/addRefereeTests.java
+++ src/TestCases/addRefereeTests.java
@@ -1,5 +1,21 @@
 package TestCases;
 
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+
+
 public class addRefereeTests {
+
+
+    @Test
+    public void test(){
+        assertEquals(2, 1+1);;
+    }
     
 }
