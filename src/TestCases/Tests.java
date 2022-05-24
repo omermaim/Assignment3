48592d74330238ee840ad8ca7aeef19402286755
hodisan94 <dhodisan@gmail.com>
2022-05-24 22:19:16 +0300

    added test class

---
+++ src/TestCases/Tests.java
@@ -0,0 +1,7 @@
+package TestCases;
+
+import Service.UserService;
+
+public abstract class Tests {
+    UserService Us = new UserService();
+}
--- src/TestCases/addRefereeTests.java
+++ src/TestCases/addRefereeTests.java
@@ -1,4 +1,4 @@
-package Tests;
+package TestCases;
 
 public class addRefereeTests {
     
