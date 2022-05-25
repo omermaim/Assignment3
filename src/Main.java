413d7d0b5050facf0228397aa7790beae33c46b3
tomerbv <tomerbv@gmail.com>
2022-05-25 17:25:40 +0300

    db 1.2

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,13 @@ public class Main {
     public static void main(String[] args) {
         DbController dbController = new DbController();
 
-//        dbController.createTables();
+        dbController.createTables();
 
-        dbController.InsertReferee("11111", "tomer wei", "050-8873928", "20/12/90");
-        System.out.println(dbController.getAllReferees());
-
-        dbController.InsertUser("omri","tomer111", "11122");
-        System.out.println(dbController.getAllUsers());
+//        dbController.InsertReferee(11111, "tomer wei", "050-8873928", "20/12/90");
+//        System.out.println(dbController.getAllReferees());
+//
+//        dbController.InsertUser("omri","tomer111", 11122);
+//        System.out.println(dbController.getAllUsers());
 
 
     }
