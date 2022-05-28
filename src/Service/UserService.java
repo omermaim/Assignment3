705643b8d7ddb26224d40f99c572de339eceb156
tomerbv <tomerbv@gmail.com>
2022-05-28 14:54:20 +0300

    userService update

--- src/Service/UserService.java
+++ src/Service/UserService.java
@@ -8,16 +8,16 @@ import java.util.ArrayList;
 public class UserService {
     system Service_controller;
 
-    public UserService(System service_controller) {
+    public UserService() {
         Service_controller = system.getinstance();
     }
     public boolean Login(String login_name,String login_password){
         return Service_controller.Login(login_name,login_password);
     }
-    public boolean addReferee(String name, String id, String password,String birthdate,String phone){
-        return Service_controller.addReferee(name,id,password,birthdate,phone);
+    public boolean addReferee(int ref_id, String name, String phoneNumber, Date birthday, String username, String password){
+        return Service_controller.addReferee(ref_id, name, phoneNumber, birthday, username, password);
     }
-    public boolean gamePlacement(int hometeam_id, int guestteam_id, ArrayList<Referee> threereferees, Date date){
-        return Service_controller.gamePlacement(hometeam_id,guestteam_id,threereferees, date);
+    public boolean gamePlacement(int game_id, Team home_team, Team guest_team, ArrayList<Referee> threereferees, Date date, String field){
+        return Service_controller.gamePlacement(game_id, home_team, guest_team, threereferees, date, field);
     }
 }
